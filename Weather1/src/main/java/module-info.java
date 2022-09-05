76cda1bc50adac5db176a4fafd3ba17380922194
Shkurko Danil <90965475+Qwolil@users.noreply.github.com>
2022-09-05 11:57:38 +0300

    Delete Weather1 directory

--- Weather1/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.weather1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires org.json;
-
-
-    opens com.example.weather1 to javafx.fxml;
-    exports com.example.weather1;
-}
